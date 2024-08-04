016245ddc819a7f96ec9109dd0d3ba8942671faa
Pete Pinter <pinter@p-squared.com>
2024-08-03 20:48:02 -0600

    Reformat view.h.

--- src/view.h
+++ src/view.h
@@ -4,39 +4,44 @@
 #include "bvh.h"
 
 /** Single bvh view */
+
 class View {
+
 	public:
+
 	enum State { EMPTY, QUEUED, LOADING, LOADED, INVALID };
 
-	View(int x, int y, int w, int h);
+	View (int x, int y, int w, int h);
 	~View();
 
-	void setBVH(BVH*, const char* name=0);
-	void resize(int x, int y, int w, int h, bool smooth=false);
-	void move(int x, int y);
-	bool contains(int mx, int my);
-	int top() const					{ return m_y; }
-	int bottom() const				{ return m_y + m_height; }
+	void setBVH 	(BVH*, const char* name=0);
+	void resize 	(int x, int y, int w, int h, bool smooth=false);
+	void move 		(int x, int y);
+	bool contains 	(int mx, int my);
 
-	void setCamera(float yaw, float pitch, float zoom);
-	void rotateView(float yaw, float pitch);
-	void zoomView(float mult);
-	void autoZoom();
+	int top() const		{ return m_y; }
+	int bottom() const	{ return m_y + m_height; }
 
-	void setVisible(bool v);
-	bool isVisible() const;
+	void setCamera 	(float yaw, float pitch, float zoom);
+	void rotateView	(float yaw, float pitch);
+	void zoomView	(float mult);
+	void autoZoom	();
 
-	void render() const;
-	void update(float time);
+	void setVisible	(bool v);
+	bool isVisible	() const;
+
+	void render		() const;
+	void update		(float time);
 	void togglePause();
 
-	State getState() const;
-	void setState(State);
+	State getState	() const;
+	void setState	(State);
 
-	void setText(const char* text);
-	static void setFont(const char* font, int size=24);
+	void setText 	(const char* text);
+	static void setFont (const char* font, int size=24);
 
 	protected:
+
 	int m_x, m_y, m_width, m_height;
 	int m_tx, m_ty, m_twidth, m_theight;
 
@@ -51,25 +56,24 @@ class View {
 
 	BVH*       m_bvh;
 	char*      m_name;
-	BVH_Math::Transform* m_final;
 	float      m_frame;
 
 	float m_projectionMatrix[16];
 	float m_viewMatrix[16];
 	float m_near, m_far;
-	BVH_Math::vec3  m_camera;
-	BVH_Math::vec3  m_target;
+
+	BVH_Math::Transform* m_final;
+	BVH_Math::vec3  	 m_camera;
+	BVH_Math::vec3  	 m_target;
 
 	protected:
-	void updateBones(float frame);
-	void updateCamera();
-	void updateProjection(float fov=90);
-	float zoomToFit(const BVH_Math::vec3& point, const BVH_Math::vec3& dir, const BVH_Math::vec3* n, float* d);
-	static void drawGrid();
-	static void drawBone();
 
+	void updateBones 		(float frame);
+	void updateCamera		();
+	void updateProjection	(float fov=90);
+	float zoomToFit			(const BVH_Math::vec3& point, const BVH_Math::vec3& dir, const BVH_Math::vec3* n, float* d);
+	static void drawGrid	();
+	static void drawBone	();
 };
 
-
 #endif
-
